043e965418d504637f7c48d546ee9e454ff2c46f
divyanshu <divyanshulohar2002@gmail.com>
2024-08-13 22:32:53 +0530

    ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import { CiLinkedin } from "react-icons/ci";
+import { FaRegEdit } from "react-icons/fa";
+import { VscGithub } from "react-icons/vsc";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="   h-[90vh] bg-black text-white   flex flex-col  items-center justify-center pb-20 ">
+      <div className="flex  rounded-3xl font-bold justify-center items-center gap-2 py-1 px-3 bg-gray-800 text-orange-500  ">
+        Sort Your Notes Easily <FaRegEdit />
+      </div>
+      <h1>Create Notes With Ease!</h1>
+      <p>
+        Streamline Your Workflow with Effortless Note Creation - Elevate
+        Productivity, Stay Organized, and Focus on What Matters Most.
+      </p>
+      <button className="w-40">Get Started</button>
+      <div className="flex">
+        <VscGithub />
+        <CiLinkedin />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FaRegMoon } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="bg-black flex  h-[10vh]">
+      <div className="w-1/2 bg-black text-3xl font-extrabold text-white   flex items-center justify-center">
+        Node<span className="text-orange-500">Nest</span>
+      </div>
+      <div className="w-1/2 text-white flex gap-5 items-center justify-center">
+        <button className="border rounded-md p-2  ">
+          <FaRegMoon color="white" size={20} />
+        </button>
+        <button className=" bg-orange-500 rounded-md p-2.5 px-3 text-sm ">
+          Sign Up
+        </button>
+
+        <button className="rounded-md p-2.5 px-3 text-sm  bg-gray-900 ">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
